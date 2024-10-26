7417d971fda17c0459ea2c2a2604e09f052ba5f8
YvanHT <yvan@hightao-mg.com>
2024-10-26 20:03:57 +0300

    feat: add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,22 @@
 export default {
   content:  [
     "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./src/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2091F9',
+        secondary: '#2DC071',
+        secondary_o: '#F15B07',
+        texte: '#252B42',
+        second_text: '#374754',
+        light_text: '#FFFFFF',
+        dark_background: '#252B42',
+        light_background: '#FFFFFF',
+        background_gradient: '#222B32'
+      },
+    },
   },
   plugins: [],
 }
