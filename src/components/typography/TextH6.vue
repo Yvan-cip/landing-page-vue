474ac07f597d5f7308f28ba292d6d72670499ef0
YvanHT <yvan@hightao-mg.com>
2024-10-27 00:19:10 +0300

    style: remove color

--- src/components/typography/TextH6.vue
+++ src/components/typography/TextH6.vue
@@ -6,9 +6,7 @@ defineProps<{
 
 <template>
   <div class="text-left p-[10px] w-full">
-    <h5
-      class="text-second_text dark:text-light_text text-[16px] leading-[23px]"
-    >
+    <h5 class="text-[16px] leading-[23px]">
       {{ title }}
     </h5>
   </div>
