5d8ec9ed54c66c9241979f3d625f5a58cf7c16e0
YvanHT <yvan@hightao-mg.com>
2024-10-27 23:31:36 +0300

    style: edit button width

--- src/components/ui/ButtonPrimary.vue
+++ src/components/ui/ButtonPrimary.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <button
-    class="text-center py-[18px] px-[38px] w-fit rounded-[35px] bg-primary"
+    class="text-center py-[18px] px-[38px] w-full rounded-[35px] bg-primary"
   >
     <span class="text-light_text text-[20px] leading-[20px]">{{ text }}</span>
   </button>
