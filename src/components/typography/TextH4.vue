cd55045b2346dc9269efdb088eb2eadec8b1d00d
YvanHT <yvan@hightao-mg.com>
2024-10-28 01:02:52 +0300

    style: add custom class in component

--- src/components/typography/TextH4.vue
+++ src/components/typography/TextH4.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 defineProps<{
   title: string
+  color?: string
 }>()
 </script>
 
 <template>
-  <div class="p-[10px] w-full">
+  <div :class="['p-[10px] w-full', color]">
     <h4 class="text-[28px] leading-[40px]">
       {{ title }}
     </h4>
