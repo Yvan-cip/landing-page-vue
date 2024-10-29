f4a84e0ebe9f45815dfd7847a4cf23ae45f660ce
YvanHT <yvan@hightao-mg.com>
2024-10-29 22:56:57 +0300

    feat: add value to input text

--- src/components/ui/InputText.vue
+++ src/components/ui/InputText.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, defineProps, defineEmits } from 'vue'
+
+const props = defineProps<{
+  label: string,
+  modelValue: string
+}>()
+
+const emit = defineEmits(['update:modelValue'])
 
 const isFocused = ref(false)
-defineProps<{
-  label: string
-}>()
+
+const updateValue = (event: Event) => {
+  emit('update:modelValue', (event.target as HTMLInputElement).value)
+}
 </script>
 
 <template>
@@ -12,14 +20,17 @@ defineProps<{
     <label
       :class="[
         'absolute left-[35px] transition-all duration-200 text-[15px]',
-        isFocused
+        isFocused || props.modelValue
           ? 'top-[-10px] bg-light_background pb-1 px-1 pt-0'
           : 'top-[20px]',
       ]"
-      >{{ label }}
+    >
+      {{ label }}
     </label>
     <input
       type="text"
+      :value="props.modelValue"
+      @input="updateValue"
       @focus="isFocused = true"
       @blur="isFocused = false"
       class="border border-[#E8E8E8] rounded-[39px] w-full bg-[#F5F5F5] px-[35px] py-[19px]"
